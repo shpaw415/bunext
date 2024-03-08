ae2c6844ba0ee80b6c28b63d97b3a534dcd18412
justin halle <shpaw415@gmail.com>
2024-03-08 08:44:16 -0500

    remove exemple

--- database/index.ts
+++ database/index.ts
@@ -1,12 +1,7 @@
 import { Table } from "./class";
+
 ("<Bunext_TypeImposts>");
-import type { _Users, _purchase, _test } from "./database_types.ts";
 ("<Bunext_TypeImposts>");
 
 ("<Bunext_DBExport>");
-export const database = {
- Users: new Table<_Users>({ name: "Users" }),
- purchase: new Table<_purchase>({ name: "purchase" }),
- test: new Table<_test>({ name: "test" }) 
-} as const;
 ("<Bunext_DBExport>");
