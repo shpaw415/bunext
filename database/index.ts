588521f58036d3e39ba7c78a3a9e17ebd0340ca5
justin halle <shpaw415@gmail.com>
2024-03-09 08:57:23 -0500

    update database integration

--- database/index.ts
+++ database/index.ts
@@ -2,7 +2,8 @@
 ("<Bunext_TypeImposts>");
 
 export async function Database() {
-  if (typeof window !== "undefined") return {};
+  if (typeof window !== "undefined")
+    throw new Error(`you cannot call database in a Client Context`);
   const Table = (await import("./class")).Table;
   ("<Bunext_DBExport>");
   ("<Bunext_DBExport>");
