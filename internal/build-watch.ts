57ac5393084e65df8d0e080c83ae11eb25673765
justin halle <shpaw415@gmail.com>
2024-03-09 09:14:19 -0500

    test build

--- internal/build-watch.ts
+++ internal/build-watch.ts
@@ -3,13 +3,15 @@ import { paths } from "../globals";
 import { sendSignal } from "../dev/dev";
 export const doWatchBuild = () =>
   watchBuild(async () => {
-    if (doBuild() === 101) process.exit(101);
+    console.log(await doBuild());
+    //if (doBuild() === 101) process.exit(101);
     sendSignal();
   }, [paths.basePagePath]);
 
-function doBuild() {
-  return Bun.spawnSync({
+async function doBuild() {
+  const proc = Bun.spawn({
     cmd: ["bun", `${paths.bunextModulePath}/internal/build.ts`],
-    stdout: "ignore",
-  }).exitCode;
+  });
+  await proc.exited;
+  return proc.exitCode;
 }
