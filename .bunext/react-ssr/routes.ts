3a63792b279c72bc3c98938aa714afe14dd25f13
justin halle <shpaw415@gmail.com>
2024-05-29 08:15:18 -0400

    update router problem

--- .bunext/react-ssr/routes.ts
+++ .bunext/react-ssr/routes.ts
@@ -2,7 +2,7 @@ import { StaticRouters } from "@bunpmjs/bunext/bun-react-ssr";
 import { mkdirSync } from "node:fs";
 import "@bunpmjs/bunext/internal/server_global";
 
-export let router = undefined as unknown as StaticRouters;
+export let router = resetRouter() as StaticRouters;
 
 export function resetRouter() {
   try {
@@ -19,14 +19,12 @@ export function resetRouter() {
         ssrMode: "nextjs",
       }
     );
-    router = route;
-    return true;
+    return route;
   } catch {
     console.log("cannot set static router");
     mkdirSync(".bunext/build/src/pages", { recursive: true });
-    return false;
+    Bun.sleepSync(1000);
+    return resetRouter();
   }
 }
-if (!resetRouter()) resetRouter();
-
 await router?.InitServerActions();
