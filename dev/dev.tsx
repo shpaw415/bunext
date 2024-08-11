74e16b28d52c77507a8f736ca1a67af55dd45bdf
justin halle <shpaw415@gmail.com>
2024-08-11 15:11:14 -0400

    test fix regression on production build

--- dev/dev.tsx
+++ dev/dev.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ReloadContext } from "../internal/router/index";
 
 declare global {
@@ -7,23 +7,32 @@ declare global {
 globalThis.__BUNEXT_DEV_INIT ??= true;
 
 export function Dev() {
-  if (typeof window === "undefined" || !globalThis.__BUNEXT_DEV_INIT)
-    return <></>;
-  else globalThis.__BUNEXT_DEV_INIT = false;
   const reload = useContext(ReloadContext);
-  const p = window.location;
-  const ws = new WebSocket(
-    `${p.protocol.includes("https") ? "wss" : "ws"}://${p.hostname}:${3001}`
-  );
-  ws.addEventListener("message", (ev) => {
-    if (ev.data != "reload") return;
-    try {
-      reload();
-    } catch {
-      window.location.reload();
-    }
-  });
-  ws.addEventListener("close", () => window.location.reload());
-  globalThis.webSocket = ws;
+  const [opened, setOpen] = useState(false);
+  const [retry, setRetry] = useState(false);
+  const [_ws, setWs] = useState<WebSocket>();
+  useEffect(() => {
+    if (globalThis.__NODE_ENV__ != "development" || opened) return;
+    if (_ws) _ws.close();
+    const p = window.location;
+    const ws = new WebSocket(
+      `${p.protocol.includes("https") ? "wss" : "ws"}://${p.hostname}:${3001}`
+    );
+    ws.addEventListener("message", (ev) => {
+      if (ev.data != "reload") return;
+      try {
+        reload();
+      } catch {
+        window.location.reload();
+      }
+    });
+    ws.addEventListener("open", () => setOpen(true));
+    ws.addEventListener("close", () => setOpen(false));
+    ws.addEventListener("error", () => setOpen(false));
+    setWs(ws);
+  }, [opened]);
+  if (!opened && _ws && _ws.readyState == 0)
+    setTimeout(() => setRetry(!retry), 5000);
+
   return <></>;
 }
