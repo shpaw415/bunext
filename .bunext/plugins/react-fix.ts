f61e509b9b6b66a95c17b7a997240c43b1b71d64
justin halle <shpaw415@gmail.com>
2024-11-06 13:32:21 -0500

    react-import-fix should have been global instead of globalThis

--- .bunext/plugins/react-fix.ts
+++ .bunext/plugins/react-fix.ts
@@ -1,14 +1,14 @@
 import { plugin, type BunPlugin } from "bun";
 
 const ToAdd = `
-globalThis.jsxDEV_7x81h0kn = (await import("react/jsx-dev-runtime")).jsxDEV;
-globalThis.jsx_w77yafs4 = (await import("react/jsx-runtime")).jsx;
-globalThis.jsxs_eh6c78nj = (await import("react/jsx-runtime")).jsxs;
-globalThis.Fragment_8vg9x3sq = (await import("react/jsx-dev-runtime")).Fragment;
+global.jsxDEV_7x81h0kn = (await import("react/jsx-dev-runtime")).jsxDEV;
+global.jsx_w77yafs4 = (await import("react/jsx-runtime")).jsx;
+global.jsxs_eh6c78nj = (await import("react/jsx-runtime")).jsxs;
+global.Fragment_8vg9x3sq = (await import("react/jsx-dev-runtime")).Fragment;
 `;
 
 const reactImportFix: BunPlugin = {
-  name: "SVG loader",
+  name: "React-import-Fix",
   setup(runtime) {
     runtime.onLoad(
       {
